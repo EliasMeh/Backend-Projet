30bfc8018183f41b30d0a94799150bcf9e9c2058
Elias MEHDAOUI- -JORGE <elias.mehdaoui-jorge@supdevinci-edu.fr>
2024-12-06 13:41:36 +0100

    Ajout des lobbies(différentes parties)

--- main.js
+++ main.js
@@ -15,35 +15,64 @@ const io = socketIo(server, {
 
 const PORT = process.env.PORT || 3001;
 
-let users = {};
-let targetNumber = Math.floor(Math.random() * 10) + 1;
+let lobbies = {
+  'general': { users: {}, targetNumber: Math.floor(Math.random() * 10) + 1 }
+};
 
 io.on('connection', (socket) => {
   console.log('New client connected');
+  let currentLobby = null;
 
-  socket.on('join', (username) => {
-    users[socket.id] = { username, score: 0 };
-    io.emit('updateUsers', Object.values(users));
+  socket.on('join', ({ username, lobby = 'general' }) => {
+    // If user was in a previous lobby, remove them from it
+    if (currentLobby) {
+      socket.leave(currentLobby);
+      delete lobbies[currentLobby].users[socket.id];
+      io.to(currentLobby).emit('updateUsers', Object.values(lobbies[currentLobby].users));
+    }
+
+    if (!lobbies[lobby]) {
+      lobbies[lobby] = { users: {}, targetNumber: Math.floor(Math.random() * 10) + 1 };
+    }
+    
+    socket.join(lobby);
+    currentLobby = lobby;
+    lobbies[lobby].users[socket.id] = { username, score: 0 };
+    io.to(lobby).emit('updateUsers', Object.values(lobbies[lobby].users));
+    io.emit('updateLobbies', Object.keys(lobbies));
   });
 
-  socket.on('guess', (guess) => {
-    const user = users[socket.id];
-    if (user) {
-      if (parseInt(guess) === targetNumber) {
+  socket.on('guess', ({ guess, lobby }) => {
+    if (lobbies[lobby] && lobbies[lobby].users[socket.id]) {
+      const user = lobbies[lobby].users[socket.id];
+      if (parseInt(guess) === lobbies[lobby].targetNumber) {
         user.score++;
         socket.emit('guessResult', 'Correct Guess!');
-        targetNumber = Math.floor(Math.random() * 10) + 1;
-        io.emit('updateUsers', Object.values(users));
+        lobbies[lobby].targetNumber = Math.floor(Math.random() * 10) + 1;
+        io.to(lobby).emit('updateUsers', Object.values(lobbies[lobby].users));
       } else {
         socket.emit('guessResult', 'Incorrect Guess');
       }
     }
   });
 
+  socket.on('createLobby', (lobbyName) => {
+    if (!lobbies[lobbyName]) {
+      lobbies[lobbyName] = { users: {}, targetNumber: Math.floor(Math.random() * 10) + 1 };
+      io.emit('updateLobbies', Object.keys(lobbies));
+    }
+  });
+
   socket.on('disconnect', () => {
     console.log('Client disconnected');
-    delete users[socket.id];
-    io.emit('updateUsers', Object.values(users));
+    if (currentLobby) {
+      delete lobbies[currentLobby].users[socket.id];
+      io.to(currentLobby).emit('updateUsers', Object.values(lobbies[currentLobby].users));
+      if (Object.keys(lobbies[currentLobby].users).length === 0 && currentLobby !== 'general') {
+        delete lobbies[currentLobby];
+      }
+      io.emit('updateLobbies', Object.keys(lobbies));
+    }
   });
 });
 
